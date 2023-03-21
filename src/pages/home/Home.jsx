9c01c7927f24bdb040d7e1b7992e6af3ee002096
indika02 <indikai002@gmail.com>
2023-03-21 10:32:16 +0530

    Featured photo created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import Featured from '../../components/featured/Featured';
 import './home.css';
+import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 const Home = () => {
   return (
@@ -11,6 +13,9 @@ const Home = () => {
     <div className='homeContainer'>
       <Featured/>
       <h1 className='homeTitle'>Browse by property type</h1>
+      <PropertyList/>
+      <h1 className='homeTitle'>homes guests love</h1>
+      <FeaturedProperties/>
     </div>
     </div>
   )
